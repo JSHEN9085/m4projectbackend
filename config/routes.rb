83d06f39b29562943224155b75f38d8b9497e3aa
github username <github email address>
2018-10-21 22:07:55 -0400

    Adjusted migration, model, controller, seedData

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :recipeingredients
-  resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :recipes,:collections
