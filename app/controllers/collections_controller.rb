590a1bbad3bbe55ceb872b146db3cfd709689324
github username <github email address>
2018-10-22 16:43:51 -0400

    4:43 PM

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,4 +4,18 @@ class CollectionsController < ApplicationController
     @recipes = @user.collected_recipes
     render json: @recipes, status: :accepted
   end
+
+  def create
+     @collection=Collection.new(
+       collector_id: params[:user_id],
+       recipe_id: params[:recipe_id]
+
+     )
+
+     if @collection.save
+       render json: @collection, status: :accepted
+     else
+       render json: { errors: @collection.errors.full_messages }, status: :unprocessible_entity
+     end
+   end
 end
